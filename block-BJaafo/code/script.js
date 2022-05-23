aa1185208f93aa9374f899509c196e6e6937445d
SMITHAN KUMAR R <smithankumar404@gmail.com>
2022-05-23 16:36:56 +0530

    test case assignment practice

--- block-BJaafo/code/script.js
+++ block-BJaafo/code/script.js
@@ -8,10 +8,30 @@ if(firstResult === expected){
 } else {
     console.warn("Test Case Failed")
 }
+// passed test case
 let finalResult = fullName("amar", "prasad");
-let finalexpected = "amar prasad";
-if(finalResult === finalexpected){
+let finalExpected = "amar prasad";
+if(finalResult === finalExpected){
     console.log("Test case Passed");
 } else {
     console.warn("Test Case Failed")
+}
+// problem 2
+function totalAmount(amount =" ",taxRate = ""){
+    return amount + (amount * taxRate);
+}
+let count = totalAmount(50000,3);
+let countExpected = 20000;
+if(count === countExpected){
+    console.log(`Total amount is ${count}`)
+}else {
+    console.warn(" not expected result try again!")
+}
+// passed test case 
+let finalCount = totalAmount(5000,3);
+let finalCountExpected = 20000;
+if(finalCount === finalCountExpected){
+    console.log(`Total amount is ${finalCount}`)
+}else {
+    console.warn(" not expected result try again!")
 }
