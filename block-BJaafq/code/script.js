584d3304aefc8cb444b33b9a98dcce51ff01b003
SMITHAN KUMAR R <smithankumar404@gmail.com>
2022-05-24 12:49:27 +0530

    working on testing assertion practice

--- block-BJaafq/code/script.js
+++ block-BJaafq/code/script.js
@@ -15,21 +15,33 @@ function test(message, callback){
         console.log(`test failed not as expected`, message)
     }
 }
+// testing assertion
+function expect(actual){
+    return {
+        toEqual: function(expected) {
+            if(actual !== expected){
+                throw new Error(`${result} is not equal to ${expected}`);
+            }
+        }, toBeEqual : function(expected){
+            if(actual !== expected){
+                throw new Error(`${result} is not equal to ${expected}`);
+            }
+        }
+    }
+}
 let result , expected;
 function testAdd() {
     result = sum(20,40);
     expected = 60;
-    if(result !== expected){
-        throw new Error(`${result} is not equal to ${expected}`);
-    }
+    expect(result).toEqual(expected);
 }
 test(`adding 2 and 4`, testAdd);
 
 function testMultiply(){
-  result = multiply(3,6);
-  expected = 18;
-  if(result !== expected){
-      throw new Error(`${result} is not equal to ${expected}`);
-  }
+  result = multiply(5,6);
+  expected = 30;
+  expect(result).toBeEqual(expected);
+
+ 
 }
 test('multiply 3 and 6' , testMultiply);
